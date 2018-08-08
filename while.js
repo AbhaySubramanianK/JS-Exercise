4ee95d8258f2f8b5d75720e605804592bba8b9f5
Abhay <abhayk97@gmail.com>
2018-08-08 19:15:51 +0530

    Commit 1

---
+++ while.js
@@ -0,0 +1,32 @@
+console.log("NOS BTW -10 and 19");
+var num = -10;
+while(num<=19){
+	console.log("num");
+	num++;
+} 
+
+console.log("EVEN NOS BTW 10 and 40");
+var num = 10;
+while(num<=40){
+	if(num%2==0){
+		console.log(num)
+	}
+	num++;
+}
+
+console.log(" ODD NOS BTW 300 and 333");
+var num = 300;
+while(num<=333){
+	if(num%2==1){
+		console.log(num);
+	}
+	num++;
+}
+console.log("NOS divisile by 5 and 3 btw 5 and 50");
+var num = 5;
+while(num<=50){
+	if(num%5==0 && num%3==0){
+		console.log(num);
+	}
+	num++;
+}
